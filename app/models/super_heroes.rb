a591b1c1ccd38c2cd7407d72aa35a83664fc6540
theadampalmer <theadampalmer@gmail.com>
2020-08-23 17:14:59 -0500

    Done.

---
+++ app/models/super_heroes.rb
@@ -0,0 +1,18 @@
+class SuperHeroes
+
+    attr_accessor :name, :power, :biography
+
+    @@heroes = []
+
+    def initialize(params)
+        @name = params[:name]
+        @power = params[:power]
+        @biography = params[:biography]
+        @@heroes << self
+    end
+
+    def self.all
+        @@heroes
+    end 
+
+end 
